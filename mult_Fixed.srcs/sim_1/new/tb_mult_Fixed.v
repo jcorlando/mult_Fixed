d22b334d3d81ba3978fc7353f659f9f4b38a766a
Juan Orlando <jcorlandomc@yahoo.com>
2020-09-07 01:25:55 -0700

    9/07/2020 01:25 pm

--- mult_Fixed.srcs/sim_1/new/tb_mult_Fixed.v
+++ mult_Fixed.srcs/sim_1/new/tb_mult_Fixed.v
@@ -8,48 +8,35 @@ parameter WI1 = 5, WF1 = 3,     // input 1 integer and fraction bits
           WFO = WF1 + WF2 + 1;  // output  integer and fraction bits
 //Inputs
 reg RESET;
-reg CLK;
 reg signed [WI1 + WF1 - 1 : 0] in1;
 reg signed [WI2 + WF2 - 1 : 0] in2;
 //Outputs
 wire signed [WIO + WFO - 1 : 0] out;
-// Clock generation
-initial CLK <= 0;
-always #50 CLK <= ~CLK;
 //Instantiate DUT
 mult_Fixed #( .WI1(WI1), .WF1(WF1), .WI2(WI2), .WF2(WF2), .WIO(WIO) )
-        DUT( .RESET(RESET), .CLK(CLK), .in1(in1), .in2(in2), .out(out) );
+        DUT( .RESET(RESET), .in1(in1), .in2(in2), .out(out) );
 
 initial
 begin
     RESET <= 0;
     in1 <= 8'b1_0110_110;
     in2 <= 9'b0_01_110011;
-    @(posedge CLK);
-    @(posedge CLK);
+    # 150;
     in1 <= 8'b0_1011_111;
     in2 <= 9'b1_11_100110;
-    @(posedge CLK);
+    # 150;
     in1 <= 8'b0_0101_100;
     in2 <= 9'b0_10_100111;
-    @(posedge CLK);
+    # 150;
     RESET <= 1;
-    @(posedge CLK);
+    # 150;
     RESET <= 0;
     in1 <= 8'b1_1111_001;
     in2 <= 9'b1_01_000011;
-    @(posedge CLK);
+    # 150;
     in1 <= 8'b0_0101_100;
     in2 <= 9'b0_01_100111;
 end
 
 endmodule
-
-
-
-
-
-
-
-
 
--- mult_Fixed.srcs/sources_1/new/mult_Fixed.v
+++ mult_Fixed.srcs/sources_1/new/mult_Fixed.v
@@ -2,21 +2,23 @@
 
 module mult_Fixed # (parameter WI1 = 4, WF1 = 4,   // input 1 integer and fraction bits
                                WI2 = 4, WF2 = 4,   // input 2 integer and fraction bits
-         WIO = WI1 + WI2 - 1, WFO = WF1 + WF2 + 1) // output  integer and fraction bits
+                            WIO = WI1 + WI2 - 1,   // output integer bits
+                            WFO = WF1 + WF2 + 1)   // output fraction bits
 (
     input RESET,
-    input CLK,
-    input signed [WI1 + WF1 - 1 : 0] in1,     // Multiplicand
-    input signed [WI2 + WF2 - 1 : 0] in2,     // Multiplier
-    output reg signed [WIO + WFO - 1 : 0] out // Output
+    input signed [WI1 + WF1 - 1 : 0] in1,           // Multiplicand
+    input signed [WI2 + WF2 - 1 : 0] in2,           // Multiplier
+    output reg signed [WIO + WFO - 1 : 0] out       // Output
 );
-    
-    
+    // Compute the full precision product
     always @ (*)
     begin
         if(RESET) out <= 0;
-        else out <= in1*in2;
+        else out <= in1 * in2;
     end
+    
+    
+    
     
 endmodule
 
